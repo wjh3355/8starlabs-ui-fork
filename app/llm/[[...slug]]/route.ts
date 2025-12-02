133d4cae03bc81b77b1269f73ecaaf45504a20ef
Tham Kei Lok <tkl48leon@gmail.com>
2025-12-02 14:20:43 +0800

    chore: ci issue

--- app/llm/[[...slug]]/route.ts
+++ app/llm/[[...slug]]/route.ts
@@ -7,9 +7,9 @@ export const revalidate = false;
 
 export async function GET(
   _req: NextRequest,
-  { params }: { params: Promise<{ slug: string[] }> }
+  { params }: { params: Promise<{ slug?: string[] }> }
 ) {
-  const slug = (await params).slug;
+  const { slug = [] } = await params;
   const page = source.getPage(slug);
 
   if (!page) {
